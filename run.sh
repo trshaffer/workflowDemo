9d42a37a7a8943d29beaf88b397ffd3d9dac1d6f
Tim Shaffer <tshaffe1@nd.edu>
2018-03-27 11:04:09 -0400

    Switch to Condor

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 # lazy option parsing: $1 must be JSON args
 
-makeflow --makeflow-log "$1.makeflowlog" --jx-args makeflows/master_args.jx makeflows/master.jx --jx-args "$@"
+/scratch365/tshaffe1/cctools/makeflow/src/makeflow --makeflow-log "$1.makeflowlog" --jx-args makeflows/master_args.jx makeflows/master.jx --jx-args "$@"
