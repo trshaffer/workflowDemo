6d2b0895a3e4fa14576bb83e5049638002c17430
Tim Shaffer <tshaffe1@nd.edu>
2018-03-28 04:25:02 -0400

    Commit Condor changes

--- run.sh
+++ run.sh
@@ -2,4 +2,8 @@
 
 # lazy option parsing: $1 must be JSON args
 
-/scratch365/tshaffe1/cctools/makeflow/src/makeflow --makeflow-log "$1.makeflowlog" --jx-args makeflows/master_args.jx makeflows/master.jx --jx-args "$@"
+cd /scratch365/tshaffe1/workflowDemo
+
+#exec > $(mktemp "../tmp/$(basename $1)".XXXXXX) 2>&1
+
+../cctools/makeflow/src/makeflow --makeflow-log "$1.makeflowlog" --jx-args makeflows/master_args.jx makeflows/master.jx --jx-args "$@"
